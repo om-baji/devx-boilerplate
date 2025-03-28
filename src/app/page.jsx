61f1e47cdf036445581282e41428167e34a8a3d9
om-baji <ombaji124@gmail.com>
2025-03-28 17:36:14 +0530

    dash

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,13 @@
 "use client";
-import { ownPokemon } from "@/actions/pokemon";
 import Navbar from "@/components/navbar";
-import PokemonCard from "@/components/poke-card";
-import { useEffect, useState } from "react";
 
 const Home = () => {
-  const [pokemonData, setPokemonData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await ownPokemon();
-      console.log(data);
-      setPokemonData(data.ownedPokemons);
-    };
-    fetchData();
-  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <Navbar />
       <main className="flex flex-col justify-center items-center h-screen">
         This is the Home page
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {pokemonData.map((pokemon) => (
-              <PokemonCard
-                key={pokemon.id}
-                pokemon={pokemon}
-              />
-            ))}
-          </div>
       </main>
     </div>
   );
